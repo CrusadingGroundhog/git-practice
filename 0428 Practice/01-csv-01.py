799a42eae8272f5ab05ca22501d7416b83649616
CrusadingGroundhog <81834635+CrusadingGroundhog@users.noreply.github.com>
2021-04-28 18:49:29 -0500

    Uploading 01-csv-01
    Uploading the Python and Jupyter files

---
+++ 0428 Practice/01-csv-01.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+cd Downloads
+
+
+# In[2]:
+
+
+pwd
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+people_df = pd.read_csv("people428.csv")
+
+
+# In[6]:
+
+
+people_df.head()
+
+
+# In[7]:
+
+
+people_df.describe()
+
+
+# In[ ]:
+
+
+
+
